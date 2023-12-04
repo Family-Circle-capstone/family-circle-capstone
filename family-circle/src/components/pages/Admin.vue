f747eb91c72b98b33a2a32323ff7cd5f4ed3f833
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-12-03 18:35:36 -0600

    added documentation

--- family-circle/src/components/pages/Admin.vue
+++ family-circle/src/components/pages/Admin.vue
@@ -9,19 +9,26 @@ const count = ref(0)
 </script>
 
 <template>
+  <!-- page container -->
   <div class="mx-auto">
+    <!-- logo -->
     <div>
       <img src="../../assets/admin/logo.svg" alt="logo" class="mx-auto pb-8 w-[252.50px] h-[123px]">
     </div>
+    <!-- grid container -->
     <div class="grid grid-cols-2 gap-8 pb-8">
+      <!-- contact one container -->
       <div class="w-[368px] h-[157px] relative rounded-lg border border-black">
+        <!-- contact buttons -->
         <div class="">
+          <!-- edit button -->
           <button type="button" class="absolute top-6 right-2 text-white bg-lightblue hover:bg-darkblue font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
             </svg>
             <span class="sr-only">Edit Contact</span>
           </button>
+          <!-- delete button -->
           <button type="button" class="absolute bottom-6 right-2 text-white bg-lightblue hover:bg-darkblue font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -29,18 +36,23 @@ const count = ref(0)
             <span class="sr-only">Delete Contact</span>
           </button>
         </div>
+        <!-- contact one info -->
         <img class="object-cover w-[95px] h-[98px] left-[26px] top-[30px] absolute border border-lightblue" src="../../assets/profiles/ben.jpg" alt="profile-pic"/>
         <div class="w-[169px] h-[41px] left-[133px] top-[42px] pt-1 text-left absolute text-darkblue text-[25px] font-normal font-['Arial']">Ben</div>
         <div class="w-[169px] h-[41px] left-[133px] top-[70px] pt-2 text-left absolute text-lightblue text-xl font-normal font-['Arial']">Grandson</div>
       </div>
+      <!-- contact two container -->
       <div class="w-[368px] h-[157px] relative rounded-lg border border-black">
+        <!-- contact buttons -->
         <div class="">
+          <!-- edit button -->
           <button type="button" class="absolute top-6 right-2 text-white bg-lightblue hover:bg-darkblue font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
             </svg>
             <span class="sr-only">Edit Contact</span>
           </button>
+          <!-- delete button -->
           <button type="button" class="absolute bottom-6 right-2 text-white bg-lightblue hover:bg-darkblue font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -48,18 +60,23 @@ const count = ref(0)
             <span class="sr-only">Delete Contact</span>
           </button>
         </div>
+        <!-- contact two info -->
         <img class="object-cover w-[95px] h-[98px] left-[26px] top-[30px] absolute border border-lightblue" src="../../assets/profiles/baylee.jpg" alt="profile-pic" />
         <div class="w-[169px] h-[41px] left-[133px] top-[42px] pt-1 text-left absolute text-darkblue text-[25px] font-normal font-['Arial']">Baylee</div>
         <div class="w-[169px] h-[41px] left-[133px] top-[70px] pt-2 text-left absolute text-lightblue text-xl font-normal font-['Arial']">Daughter</div>
       </div>
+      <!-- contact three container -->
       <div class="w-[368px] h-[157px] relative rounded-lg border border-black">
+        <!-- button container -->
         <div class="">
+          <!-- edit button -->
           <button type="button" class="absolute top-6 right-2 text-white bg-lightblue hover:bg-darkblue font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
             </svg>
             <span class="sr-only">Edit Contact</span>
           </button>
+          <!-- delete button -->
           <button type="button" class="absolute bottom-6 right-2 text-white bg-lightblue hover:bg-darkblue font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -67,12 +84,16 @@ const count = ref(0)
             <span class="sr-only">Delete Contact</span>
           </button>
         </div>
+        <!-- contact three info -->
         <img class="object-cover w-[95px] h-[98px] left-[26px] top-[30px] absolute border border-lightblue" src="../../assets/profiles/alex.jpg" alt="profile-pic" />
         <div class="w-[169px] h-[41px] left-[133px] top-[42px] pt-1 text-left absolute text-darkblue text-[25px] font-normal font-['Arial']">Alex</div>
         <div class="w-[169px] h-[41px] left-[133px] top-[70px] pt-2 text-left absolute text-lightblue text-xl font-normal font-['Arial']">Nephew</div>
       </div>
+      <!-- contact four container -->
       <div class="w-[368px] h-[157px] relative rounded-lg border border-black">
+        <!-- button container -->
         <div class="w-[95px] h-[98px] left-[26px] top-[30px] absolute">
+          <!-- add button -->
           <button type="button" class="absolute inset-y-4 left-2 text-white bg-lightblue hover:bg-darkblue font-medium rounded-lg text-lg p-2.5 text-center inline-flex items-center me-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-12 h-12">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -80,19 +101,24 @@ const count = ref(0)
             <span class="sr-only">Add Contact</span>
           </button>
         </div>
+        <!-- add text -->
         <div class="w-[169px] h-[41px] left-[133px] top-[55px] pt-1 text-left absolute text-darkblue text-[25px] font-normal font-['Arial']">Add Contact</div>
       </div>
     </div>
+    <!-- admin buttons container -->
     <div class="grid grid-rows-3 gap-4 pt-8">
       <div class="space-x-10">
+        <!-- save button -->
         <button type="button" class="text-white bg-darkgreen hover:bg-lightgreen rounded-lg text-md px-5 py-2.5 text-center inline-flex items-center">
           <img class="pr-3 object-fill h-10 w-10" src="../../assets/admin/save.svg" alt="save" />
           <span class="font-bold font-['Arial']">Save Changes</span>
         </button>
+        <!-- update pin button -->
         <button type="button" class="text-white bg-darkblue hover:bg-lightblue rounded-lg text-md px-5 py-2.5 text-center inline-flex items-center">
           <img class="pr-3 object-fill h-10 w-10" src="../../assets/admin/key.svg" alt="pin" />
           <span class="font-bold font-['Arial']">Update PIN</span>
         </button>
+        <!-- cancel button -->
         <button type="button" class="text-white bg-darkgrey hover:bg-lightgrey rounded-lg text-md px-5 py-2.5 text-center inline-flex items-center">
           <img class="pr-3 object-fill h-10 w-10" src="../../assets/admin/cancel.svg" alt="cancel" />
           <span class="font-bold font-['Arial']">Cancel Changes</span>
--- family-circle/src/components/pages/Home.vue
+++ family-circle/src/components/pages/Home.vue
@@ -11,7 +11,9 @@ const count = ref(0)
 
 <template>
   <div class="mx-auto">
+    <!-- contacts container -->
     <div class="grid grid-cols-3">
+      <!-- contact one button -->
       <button class="w-[300px] h-[300px] relative">
         <span class="w-[300px] h-[300px] left-0 top-0 absolute bg-white bg-opacity-0 rounded-[50px] shadow-inner border-4 border-darkblue"></span>
         <span class="w-[308px] h-[308px] left-[-4px] top-[-4px] absolute bg-white bg-opacity-0 rounded-[54px] shadow-inner border-4 border-lightblue"></span>
@@ -20,6 +22,7 @@ const count = ref(0)
         <img class="object-cover w-[150px] h-[150px] left-[75px] top-[28px] absolute rounded-full border border-lightblue" src="../../assets/profiles/ben.jpg" alt="profile-pic"/>
       </button>
       <div><!--grid spacing--></div>
+      <!-- contact two button -->
       <button class="w-[300px] h-[300px] relative">
         <span class="w-[300px] h-[300px] left-0 top-0 absolute bg-white bg-opacity-0 rounded-[50px] shadow-inner border-4 border-darkblue"></span>
         <span class="w-[308px] h-[308px] left-[-4px] top-[-4px] absolute bg-white bg-opacity-0 rounded-[54px] shadow-inner border-4 border-lightblue"></span>
@@ -28,10 +31,12 @@ const count = ref(0)
         <img class="object-cover w-[150px] h-[150px] left-[75px] top-[28px] absolute rounded-full border border-lightblue" src="../../assets/profiles/baylee.jpg" alt="profile-pic"/>
       </button>
       <div><!--grid spacing--></div>
+      <!-- logo -->
       <div>
         <img src="../../assets/logo.svg" class="mx-auto object-scale-down h-40" alt="logo">
       </div>
       <div><!--grid spacing--></div>
+      <!-- contact three button -->
       <button class="w-[300px] h-[300px] relative">
         <span class="w-[300px] h-[300px] left-0 top-0 absolute bg-white bg-opacity-0 rounded-[50px] shadow-inner border-4 border-darkblue"></span>
         <span class="w-[308px] h-[308px] left-[-4px] top-[-4px] absolute bg-white bg-opacity-0 rounded-[54px] shadow-inner border-4 border-lightblue"></span>
@@ -39,11 +44,13 @@ const count = ref(0)
         <span class="pt-2 w-[300px] h-[47px] left-0 top-[238px] absolute text-center text-darkblue text-3xl font-extralight font-['Arial']">Nephew</span>
         <img class="object-cover w-[150px] h-[150px] left-[75px] top-[28px] absolute rounded-full border border-lightblue" src="../../assets/profiles/alex.jpg" alt="profile-pic"/>
       </button>
+      <!-- admin button -->
       <div class="pt-8 flex items-end">
         <a href="Admin.vue" class="mx-auto">
           <img src="../../assets/home/gear.svg" alt="settings">
         </a>
       </div>
+      <!-- contact four button -->
       <div class="w-[300px] h-[300px] relative">
       </div>
     </div>
