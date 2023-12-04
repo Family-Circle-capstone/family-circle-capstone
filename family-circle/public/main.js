2f9fe67f14bd23d91f0ad00bc8173950c326a47b
Alex Leeper <alixvox@gmail.com>
2023-12-04 14:23:51 -0500

    twilio front/backend. Main.js allows for connection to twilio AWS

---
+++ family-circle/public/main.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("room-name-form");
+const roomNameInput = document.getElementById("room-name-input");
+const container = document.getElementById("video-container");
+
+const startRoom = async (event) => {
+  event.preventDefault();
+  form.style.visibility = "hidden";
+  const roomName = roomNameInput.value;
+
+  // Fetch an Access Token from the join-room route
+  const response = await fetch("/join-room", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ roomName: roomName }),
+  });
+  const { token } = await response.json();
+
+  // Join the video room with the token
+  const room = await joinVideoRoom(roomName, token);
+  console.log(room);
+};
+
+const joinVideoRoom = async (roomName, token) => {
+  // Join the video room with the Access Token and the given room name
+  const room = await Twilio.Video.connect(token, {
+    room: roomName,
+  });
+  return room;
+};
+
+form.addEventListener("submit", startRoom);
