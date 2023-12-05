511caa829d6e7fc8c1f7153894067cac30018d0d
Alex Leeper <alixvox@gmail.com>
2023-12-05 01:38:19 -0500

    Small change to main.js

--- family-circle/public/main.js
+++ family-circle/public/main.js
@@ -7,7 +7,6 @@ const startRoom = async (event) => {
   form.style.visibility = "hidden";
   const roomName = roomNameInput.value;
 
-  // Fetch an Access Token from the join-room route
   const response = await fetch("/join-room", {
     method: "POST",
     headers: {
@@ -18,17 +17,62 @@ const startRoom = async (event) => {
   });
   const { token } = await response.json();
 
-  // Join the video room with the token
   const room = await joinVideoRoom(roomName, token);
-  console.log(room);
+
+  // Render the local participant's video and audio tracks
+  handleConnectedParticipant(room.localParticipant);
+
+  // Render any remote participants' video and audio tracks
+  room.participants.forEach(handleConnectedParticipant);
+
+  // Setup event listeners for participant connection and disconnection
+  room.on("participantConnected", handleConnectedParticipant);
+  room.on("participantDisconnected", handleDisconnectedParticipant);
+
+  // Setup event listeners for page unload
+  window.addEventListener("pagehide", () => room.disconnect());
+  window.addEventListener("beforeunload", () => room.disconnect());
 };
 
 const joinVideoRoom = async (roomName, token) => {
-  // Join the video room with the Access Token and the given room name
   const room = await Twilio.Video.connect(token, {
     room: roomName,
   });
   return room;
+};
+
+const handleConnectedParticipant = (participant) => {
+  let participantDiv = document.getElementById(participant.identity);
+  if (!participantDiv) {
+    participantDiv = document.createElement("div");
+    participantDiv.setAttribute("id", participant.identity);
+    container.appendChild(participantDiv);
+  }
+
+  participant.tracks.forEach((trackPublication) => {
+    handleTrackPublication(trackPublication, participant);
+  });
+
+  participant.on("trackPublished", handleTrackPublication);
+};
+
+const handleTrackPublication = (trackPublication, participant) => {
+  function displayTrack(track) {
+    const participantDiv = document.getElementById(participant.identity);
+    participantDiv.append(track.attach());
+  }
+
+  if (trackPublication.track) {
+    displayTrack(trackPublication.track);
+  }
+
+  trackPublication.on("subscribed", displayTrack);
+};
+
+const handleDisconnectedParticipant = (participant) => {
+  participant.removeAllListeners();
+  const participantDiv = document.getElementById(participant.identity);
+  participantDiv.remove();
 };
 
 form.addEventListener("submit", startRoom);
