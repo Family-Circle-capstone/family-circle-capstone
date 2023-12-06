4bb7d9b7a72179ec226c4988cdaf28ce51f171ce
mdallen5393 <allen_matthew@outlook.com>
2023-12-06 12:55:40 -0600

    Added basic video call screen and retrieved data from localstorage

--- family-circle/src/App.vue
+++ family-circle/src/App.vue
@@ -1,22 +1,14 @@
 <script setup>
-import Home from './components/pages/Home.vue'
-import Admin from './components/pages/Admin.vue'
+  let contacts = [
+    { name: 'Ben Sbanotto', relation: 'nephew', img_src: './'},
+    { name: 'Baylee Bowen', relation: 'daughter', img_src: './'},
+    { name: 'Alex Leeper', relation: 'grandson', img_src: './'}
+  ]
 
-let contacts = [
-  { name: 'Ben Sbanotto', relation: 'nephew', img_src: './'},
-  { name: 'Baylee Bowen', relation: 'daughter', img_src: './'},
-  { name: 'Alex Leeper', relation: 'grandson', img_src: './'}
-]
-
-localStorage.setItem('contacts', JSON.stringify(contacts));
-
-let storedContacts = JSON.parse(localStorage.getItem('contacts'));
-console.log(storedContacts);
+  localStorage.setItem('contacts', JSON.stringify(contacts));
 </script>
 
 <template>
   <router-view>
   </router-view>
-  <!-- <Home /> -->
 </template>
-
--- family-circle/src/main.js
+++ family-circle/src/main.js
@@ -1,17 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './components/pages/Home.vue';
 import Admin from './components/pages/Admin.vue';
+import VideoCall from './components/pages/VideoCall.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: Home },
         { path: '/admin', component: Admin },
-        // { path: '/video', component: Video }
+        { path: '/video-call/:contactIndex', component: VideoCall },
     ]
 })
 
