0b3442d97ef08a97d9da5eb916b1ad381d9d4021
Alex Leeper <alixvox@gmail.com>
2023-12-05 20:39:13 -0500

    adding change to postbuild.sh

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Navigate to the root directory of the project
+cd "$(dirname "$0")/.." || exit
+
 rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute/default
 
+# Copy the built Vue application and server files
 cp -r ./family-circle/dist ./.amplify-hosting/compute/default/dist
 cp ./family-circle/server.js ./.amplify-hosting/compute/default/server.js
 cp -r ./family-circle/node_modules ./.amplify-hosting/compute/default/node_modules
 
+# Copy static assets
 cp -r ./family-circle/public ./.amplify-hosting/static
 
+# Copy the deployment manifest
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
