380202e760e7669b1d4626cbb22f48671ff2740f
mdallen5393 <allen_matthew@outlook.com>
2023-12-06 09:36:28 -0600

    Working on localstorage of contacts

--- family-circle/src/App.vue
+++ family-circle/src/App.vue
@@ -1,6 +1,17 @@
 <script setup>
 import Home from './components/pages/Home.vue'
 import Admin from './components/pages/Admin.vue'
+
+let contacts = [
+  { name: 'Ben Sbanotto', relation: 'nephew', img_src: './'},
+  { name: 'Baylee Bowen', relation: 'daughter', img_src: './'},
+  { name: 'Alex Leeper', relation: 'grandson', img_src: './'}
+]
+
+localStorage.setItem('contacts', JSON.stringify(contacts));
+
+let storedContacts = JSON.parse(localStorage.getItem('contacts'));
+console.log(storedContacts);
 </script>
 
 <template>
