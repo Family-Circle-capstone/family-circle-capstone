e185e3887c5b82391a4bc12eec4dea7f4150ff2c
Alex Leeper <alixvox@gmail.com>
2023-12-09 02:56:02 -0600

    reconfiguring VideoCall.vue to include all video func

--- family-circle/src/components/pages/VideoCall.vue
+++ family-circle/src/components/pages/VideoCall.vue
@@ -1,24 +1,108 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 import { useRoute } from 'vue-router';
+import TwilioVideo from 'twilio-video';
 import EndCallButton from '../buttons/video_call/EndCall.vue';
 import VolumePanel from '../buttons/video_call/VolumePanel.vue';
-// import LocalCamera from '../cameras/LocalCamera.vue';
-// import RemoteCamera from '../cameras/RemoteCamera.vue';
 
 let storedContacts = ref([]);
 let relation = ref('');
 let name = ref('');
+const localVideoRef = ref(null);
+const remoteVideoRef = ref(null);
+const route = useRoute();
+const roomName = route.params.contactIndex;
+let room = ref(null);
+
+async function fetchAccessToken(roomName) {
+    const response = await fetch('/join-room', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ roomName }),
+    });
+    const { token } = await response.json();
+    return token;
+}
+
+async function connectToRoom(token) {
+    try {
+        room = await TwilioVideo.connect(token, {
+            room: roomName,
+        });
+        setupRoomEventListeners();
+        if (room.value) {
+            handleConnectedParticipant(room.localParticipant);
+            room.participants.forEach(handleConnectedParticipant);
+        }
+    } catch (error) {
+        console.error('Failed to connect to Twilio Room:', error);
+    }
+}
+
+function setupRoomEventListeners() {
+    if (room) {
+        room.on("participantConnected", handleConnectedParticipant);
+        room.on("participantDisconnected", handleDisconnectedParticipant);
+        window.addEventListener("pagehide", () => room.disconnect());
+        window.addEventListener("beforeunload", () => room.disconnect());
+    }
+}
+
+function handleConnectedParticipant(participant) {
+    if (participant) {
+        participant.tracks.forEach((trackPublication) => {
+            if (trackPublication.track) {
+                displayTrack(trackPublication.track, participant)
+            }
+        });
+        participant.on("trackPublished", (trackPublication) => {
+            if (trackPublication.track) {
+                displayTrack(trackPublication.track, participant)
+            }
+        });
+    }
+}
+
+function displayTrack(track, participant) {
+    if (track.kind === "video") {
+        const videoElement = track.attach();
+        if (participant === room.localParticipant) {
+            localVideoRef.value.appendChild(videoElement);
+        } else {
+            remoteVideoRef.value.appendChild(videoElement);
+        }
+    }
+}
+
+function handleDisconnectedParticipant(participant) {
+    participant.removeAllListeners();
+    const participantDiv = document.getElementById(participant.identity);
+    if (participantDiv) {
+        participantDiv.remove();
+    }
+}
 
 onMounted(() => {
-    let route = useRoute();
-    storedContacts.value = JSON.parse(localStorage.getItem('contacts')) || [];
-    let contactIndex = route.params.contactIndex;
-    if (storedContacts.value.length > contactIndex) {
-        relation.value = storedContacts.value[contactIndex].relation;
-        name.value = storedContacts.value[contactIndex].name;
+    const initializeRoom = async () => {
+        storedContacts.value = JSON.parse(localStorage.getItem('contacts')) || [];
+        if (storedContacts.value.length > roomName) {
+            relation.value = storedContacts.value[roomName].relation;
+            name.value = storedContacts.value[roomName].name;
+        }
+        const token = await fetchAccessToken(roomName);
+        await connectToRoom(token);
+    };
+    initializeRoom();
+});
+
+onUnmounted(() => {
+    if (room) {
+        room.disconnect();
     }
 });
+
 </script>
 
 <template>
@@ -28,12 +112,12 @@ onMounted(() => {
             You are speaking to your {{ relation }}, {{ name }}.
         </h1>
         <!-- Local User's Camera -->
-        <div class="row-start-1 row-end-2 col-start-3 col-end-4 bg-lightgrey">
-            <h1>Local</h1>
+        <div ref="localVideoRef" class="row-start-1 row-end-2 col-start-3 col-end-4 bg-lightgrey">
+            <!-- Local video will be attached here -->
         </div>
         <!-- Remote User's Camera -->
-        <div class="row-start-2 row-end-4 col-start-1 col-end-3 bg-darkgrey">
-            <h1>Remote</h1>
+        <div ref="remoteVideoRef" class="row-start-2 row-end-4 col-start-1 col-end-3 bg-darkgrey">
+            <!-- Remote video will be attached here -->
         </div>
         <!-- Volume Buttons -->
         <button class="row-start-2 row-end-3 col-start-3 col-end-4 bg-lightblue">
