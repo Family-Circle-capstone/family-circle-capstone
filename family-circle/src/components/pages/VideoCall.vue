ed5790efda89927c516de15e6a2370f0e0ce09dc
Alex Leeper <alixvox@gmail.com>
2023-12-08 23:59:12 -0600

    Changing VideoCall to implement grid change

--- family-circle/src/components/pages/VideoCall.vue
+++ family-circle/src/components/pages/VideoCall.vue
@@ -1,36 +1,46 @@
 <script setup>
-    import { ref, onMounted } from 'vue';
-    import { useRoute } from 'vue-router';
-    import EndCallButton from '../buttons/video_call/EndCall.vue';
-    import VolumePanel from '../buttons/video_call/VolumePanel.vue';
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import EndCallButton from '../buttons/video_call/EndCall.vue';
+import VolumePanel from '../buttons/video_call/VolumePanel.vue';
+// import LocalCamera from '../cameras/LocalCamera.vue';
+// import RemoteCamera from '../cameras/RemoteCamera.vue';
 
-    let storedContacts = ref([]);
-    let relation = ref('');
-    let name = ref('');
+let storedContacts = ref([]);
+let relation = ref('');
+let name = ref('');
 
-    onMounted(() => {
-        let route = useRoute(); // Get the current route object
-        storedContacts.value = JSON.parse(localStorage.getItem('contacts')) || [];
-        let contactIndex = route.params.contactIndex; // Get the contact index from the route parameters
-        if (storedContacts.value.length > contactIndex) {
-            relation.value = storedContacts.value[contactIndex].relation;
-            name.value = storedContacts.value[contactIndex].name;
-        }
-    });
+onMounted(() => {
+    let route = useRoute();
+    storedContacts.value = JSON.parse(localStorage.getItem('contacts')) || [];
+    let contactIndex = route.params.contactIndex;
+    if (storedContacts.value.length > contactIndex) {
+        relation.value = storedContacts.value[contactIndex].relation;
+        name.value = storedContacts.value[contactIndex].name;
+    }
+});
 </script>
 
 <template>
-    <div class="grid grid-rows-4 grid-cols-2 gap-4 w-[80rem] h-screen">
-        <button class="row-start-1 row-end-3 col-start-0 col-end-1 bg-lightblue">
-            <VolumePanel/>
-        </button>
+    <div class="grid grid-rows-3 grid-cols-3 gap-4 w-[40rem] h-screen">
+        <!-- Text Section -->
         <h1 class="row-start-1 row-end-2 col-start-1 col-end-3 bg-darkblue">
             You are speaking to your {{ relation }}, {{ name }}.
         </h1>
-        <div class="row-start-2 row-end-5 col-start-1 col-end-3 bg-darkgrey">
-            Video
+        <!-- Local User's Camera -->
+        <div class="row-start-1 row-end-2 col-start-3 col-end-4 bg-lightgrey">
+            <h1>Local</h1>
         </div>
-        <button class="row-start-4 row-end-5 col-start-0 col-end-1 bg-darkred">
+        <!-- Remote User's Camera -->
+        <div class="row-start-2 row-end-4 col-start-1 col-end-3 bg-darkgrey">
+            <h1>Remote</h1>
+        </div>
+        <!-- Volume Buttons -->
+        <button class="row-start-2 row-end-3 col-start-3 col-end-4 bg-lightblue">
+            <VolumePanel/>
+        </button>
+        <!-- End Call Button -->
+        <button class="row-start-3 row-end-4 col-start-3 col-end-4 bg-darkred">
             <EndCallButton/>
         </button>
     </div>
