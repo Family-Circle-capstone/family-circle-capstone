98e4e7de95e7bc8ac5574cf7c775ff1a1df27da5
Alex Leeper <alixvox@gmail.com>
2023-12-11 10:10:00 -0600

    fixing typo in postbuild.sh

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -11,7 +11,7 @@ mkdir -p ./.amplify-hosting/compute/default
 cp -r ./family-circle/dist ./.amplify-hosting/compute/default/dist
 cp ./family-circle/server.js ./.amplify-hosting/compute/default/server.js
 cp ./family-circle/package.json ./.amplify-hosting/compute/default/package.json
-cp ./family-circle/.env.production ./.amplift-hosting/compute/default/.env.production
+cp ./family-circle/.env.production ./.amplify-hosting/compute/default/.env.production
 cp -r ./family-circle/node_modules ./.amplify-hosting/compute/default/node_modules
 
 # Copy the deployment manifest
