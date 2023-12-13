4a58168096862230ce49d8f9cfcaaeaa37d5149a
Baylee Bowen <90226747+b-duncs@users.noreply.github.com>
2023-12-12 22:59:51 -0600

    Update VideoCall.vue

--- family-circle/src/components/pages/VideoCall.vue
+++ family-circle/src/components/pages/VideoCall.vue
@@ -47,6 +47,7 @@ async function connectToRoom(token) {
   try {
     let connectedRoom = await TwilioVideo.connect(token, {
       room: roomName,
+      video: { width: 640 }
     });
     connectedRoom = markRaw(connectedRoom);
     room.value = connectedRoom;
