1a10e07803dfe04706455b1c7b94257f090f8e37
Baylee Bowen <90226747+b-duncs@users.noreply.github.com>
2023-12-14 22:18:26 -0600

    updated admin buttons

--- family-circle/src/components/buttons/admin/UpdatePin.vue
+++ family-circle/src/components/buttons/admin/UpdatePin.vue
@@ -1,5 +1,5 @@
 <template>
-  <div  class="text-white bg-lightblue hover:bg-darkblue rounded-lg text-md px-5 py-2.5 text-center inline-flex items-center">
+  <div  class="w-[190px] text-white bg-lightblue hover:bg-darkblue rounded-lg text-md px-5 py-2.5 text-center inline-flex items-center">
     <img class="pr-3 object-fill h-10 w-10" src="../../../assets/admin/key.svg" alt="pin" />
     <span class="font-bold font-['Arial']">Update PIN</span>
   </div>
