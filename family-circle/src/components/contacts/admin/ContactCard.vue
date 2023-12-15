cac00b7d4a2ed567711f0c38638429e77629951a
Baylee Bowen <90226747+b-duncs@users.noreply.github.com>
2023-12-14 19:12:55 -0600

    updated text font on admin page

--- family-circle/src/components/contacts/admin/ContactCard.vue
+++ family-circle/src/components/contacts/admin/ContactCard.vue
@@ -32,8 +32,8 @@ function fetchContact() {
       </div>
       <!-- contact one info -->
       <img class="object-cover w-[95px] h-[98px] left-[26px] top-[30px] absolute border border-lightblue" :src="contact.img_src" alt="profile-pic"/>
-      <div class="w-[169px] h-[41px] left-[133px] top-[42px] pt-1 text-left absolute text-darkblue text-[25px] font-normal font-['Arial']">{{ contact.name }}</div>
-      <div class="w-[169px] h-[41px] left-[133px] top-[70px] pt-2 text-left absolute text-lightblue text-xl font-normal font-['Arial']">{{ contact.relation }}</div>
+      <div class="w-[169px] h-[41px] left-[133px] top-[42px] pt-1 text-left absolute text-darkblue text-[25px] font-normal font-['Roboto-Serif']">{{ contact.name }}</div>
+      <div class="w-[169px] h-[41px] left-[133px] top-[70px] pt-2 text-left absolute text-lightblue text-xl font-normal font-sans">{{ contact.relation }}</div>
     </div>
   </div>
 </template>
