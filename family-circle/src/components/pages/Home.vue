bfe953f54b723bddf65fb96a513c6f963cb95c4e
bsbanotto <ben.sbanotto@gmail.com>
2023-12-15 11:52:14 -0600

    Slamming voice recognition back in there!

--- family-circle/src/components/pages/Home.vue
+++ family-circle/src/components/pages/Home.vue
@@ -1,17 +1,74 @@
 <script setup>
-import ContactCard from '../contacts/home/ContactCard.vue';
-import EnterPinModal from "../modals/home/EnterPin.vue";
+  import ContactCard from '../contacts/home/ContactCard.vue';
+  import EnterPinModal from "../modals/home/EnterPin.vue";
+  import { onMounted, ref } from 'vue';
+  import { useRouter } from 'vue-router';
 
-let contacts = JSON.parse(localStorage.getItem('contacts'));
+  const router = useRouter();
 
-// Computed property to determine the component type and additional properties
-const getComponentInfo = (contact) => {
-  if (contact.name) {
-    return { type: ContactCard };
-  } else {
-    return { type: 'empty', width: '300px', height: '300px' };
-  }
-};
+  // Use a ref to store the recognized command
+  const voiceCommand = ref('');
+
+  // Listen for the custom voiceCommand event
+  onMounted(() => {
+    document.addEventListener('voiceCommand', handleVoiceCommand);
+  });
+
+  let contacts = JSON.parse(localStorage.getItem('contacts'));
+
+  const handleVoiceCommand = (event) => {
+    voiceCommand.value = event.detail;
+
+    console.log('Heard command:', voiceCommand.value);
+
+    // Check if the command starts with "circle call"
+    if (voiceCommand.value.startsWith('circle call')) {
+      // Extract the contact name from the command
+      const contactNameInput = voiceCommand.value.replace('circle call', '').trim().toLowerCase();
+
+      console.log('Contact name input:', contactNameInput);
+
+      // Split the input into first and last names
+      const [inputFirstName, inputLastName] = contactNameInput.split(' ');
+
+      // Find the contact in the contacts array with priority to full name, then first name, then last name
+      const contactIndex = contacts.findIndex((c) => {
+        const lowercasedFullName = c.name.toLowerCase();
+        const [contactFirstName, contactLastName] = c.name.split(' ').map(name => name.toLowerCase());
+
+        console.group(`Testing contact: ${c.name}`);
+        console.log(`Input vs. Full name: ${contactNameInput} vs. ${lowercasedFullName}:`, lowercasedFullName === contactNameInput);
+        console.log(`Input first name vs. Contact first name: ${inputFirstName} vs. ${contactFirstName}:`, contactFirstName.includes(inputFirstName));
+        console.log(`Input last name vs. Contact last name: ${inputLastName} vs. ${contactLastName}:`, contactLastName.includes(inputLastName));
+        console.groupEnd();
+
+        return (
+          lowercasedFullName === contactNameInput ||
+          contactFirstName.includes(inputFirstName) ||
+          contactLastName.includes(inputLastName)
+        );
+      });
+
+      // If the contact is found, log the index and navigate to the video call route
+      if (contactIndex !== -1) {
+        console.log('Contact found at index:', contactIndex);
+        router.push(`/video-call/${contactIndex}`);
+      } else {
+        console.log('Contact not found.');
+      }
+    } else {
+      console.log('Invalid command format.');
+    }
+  };
+
+  // Computed property to determine the component type and additional properties
+  const getComponentInfo = (contact) => {
+    if (contact.name) {
+      return { type: ContactCard };
+    } else {
+      return { type: 'empty', width: '300px', height: '300px' };
+    }
+  };
 </script>
 
 <template>
